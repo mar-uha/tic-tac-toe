49d4a82b6176b51460577c9323c94b5a1b591d94
Marc UHART <marcuhart@gmail.com>
2023-04-11 12:10:06 -0700

    Taking turns.

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,12 +9,23 @@ function Square({value, onSquareClick}: {value: string, onSquareClick: MouseEven
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i: number) {
+    if (squares[i]) {
+      return;
+    }
+
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
